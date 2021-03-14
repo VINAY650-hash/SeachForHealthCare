53de31b7d4b6986acbdd8fe590b2d8095cafadd1
VINAY650-hash <79817305+VINAY650-hash@users.noreply.github.com>
2021-03-14 21:00:00 +0530

    Add files via upload

---
+++ Logout_Servlet.java
@@ -0,0 +1,35 @@
+
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Admin_Action_Servlet
+ */
+@WebServlet("/Logout_Servlet")
+public class Logout_Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		if (session != null) 
+		{
+		    session.invalidate();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
